bbe52fcbdb3de51c48fe0c2750d531926430d968
Hussen <hussenmohammedak@gmail.com>
2022-11-05 16:10:25 -0300

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,205 @@
+# pip install --upgrade google-api-python-client
+# pip install --upgradpe google-auth-oauthlib google-auth-httplib2
+
+from googleapiclient.discovery import build
+from functools import lru_cache
+from secret import *
+
+import os
+
+class ApiCrawler():
+
+    def __init__(self):
+        """ 
+        Initializes ApiCrawler instance with 10,000 units quote per day or 
+        250,000 comments per day. E.g. a Joe Rogan podcast episode with Jordan
+        Peterson has 89,925 comments + replies at 25,000,000 views
+        """
+        self.api_key = api_key
+
+    @lru_cache # retrieves cached output if arguments the same the second time
+    def get_comments(self, video_id, n_requested):
+      """ Gets the top-level comments of a single video and returns an 
+      array of strings
+      e.g. [ "first comment", "second comment", ...]
+      """
+
+      # intialize empty array to return comments
+      comments = []
+
+      # create youtube resource object
+      youtube = build('youtube', 'v3', developerKey = self.api_key)
+
+      # retrieve youtube video results
+      video_response = youtube.commentThreads().list(
+          maxResults = 100,
+          part = 'snippet',
+          videoId = video_id
+          ).execute()
+
+      # iterate video responses
+      while video_response:
+
+          # extract required info from each result object 
+          for item in video_response['items']:
+
+              # extract comment resources from comment threat resource
+              comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+
+              # append a single comment
+              comments.append(comment)
+
+          # repeat if next page of comments exist
+          if 'nextPageToken' in video_response:
+            
+            # don't repeat larger than number of comments requested
+            if len(comments) >= n_requested:
+              video_response = False
+
+            else:
+              video_response = youtube.commentThreads().list(
+                      part = 'snippet',
+                      videoId = video_id,
+                      pageToken = video_response['nextPageToken']
+                  ).execute()
+
+          else:
+            video_response = False
+
+            
+      # print(f"actual number: {len(comments)}")
+      # print(f"requested number: {n_requested}")
+
+      return comments
+    
+    @lru_cache
+    def get_comments_and_replies(self, video_id, n_requested):
+        """
+        Gets the top-level comments and replies of a single video and returns a 
+        nested array of strings.
+        E.g. [["first comment", ["first reply", "second reply"]], ["second
+        comment", ["first reply", "second reply"], [third...]]]
+        """
+
+        # intialize empty array to return comments
+        comments_replies = []
+  
+        # create youtube resource object
+        youtube = build('youtube', 'v3', developerKey = self.api_key)
+  
+        # retrieve youtube video results
+        video_response = youtube.commentThreads().list(
+          maxResults = 100,
+          part = 'snippet, replies',
+          videoId = video_id
+          ).execute()
+
+        # iterate video responses
+        while video_response:
+        
+            # extract required info from each result object 
+            for item in video_response['items']:
+            
+                # extract comment resources from comment threat resource
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                # count number of replies of comment
+                replycount = item['snippet']['totalReplyCount']
+
+                replies = []
+                if replycount > 0:
+                
+                    # iterate through all reply
+                    for reply in item['replies']['comments']:
+                        reply = reply['snippet']['textDisplay']
+                        replies.append(reply)
+  
+                # append a single comment and its replies
+                comments_replies.append([comment, replies])
+  
+            # repeat if next page of comments exist
+            if 'nextPageToken' in video_response:
+            
+              # don't repeat larger than number of comments requested
+              if len(comments_replies) >= n_requested:
+                video_response = False
+
+              else:
+                video_response = youtube.commentThreads().list(
+                        part = 'snippet, replies',
+                        videoId = video_id,
+                        pageToken = video_response['nextPageToken']
+                    ).execute()
+
+            else:
+              video_response = False
+
+        print(f"comment-reply bundle: {len(comments_replies)}")
+
+        return comments_replies
+
+crawler = ApiCrawler()
+# comments = crawler.get_comments("RcYjXbSJBN8") # Joe Rofan podcase with 89,925 comments + replies
+comments = crawler.get_comments_and_replies("___NoMi5pp0", 100) # random Korean video with 118 comments + replies
+print(comments)
+
+'''
+Sample commentThreads.list() JSON response
+{
+  "kind": "youtube#commentThreadListResponse",
+  "etag": etag,
+  "nextPageToken": string,
+  "pageInfo": {
+    "totalResults": integer,
+    "resultsPerPage": integer
+  },
+  "items": [
+    commentThread Resource ***
+  ]
+}
+
+Sample commentThread Resource object:
+{
+  "kind": "youtube#commentThread",
+  "etag": etag,
+  "id": string,
+  "snippet": {
+    "channelId": string,
+    "videoId": string,
+    "topLevelComment": comments Resource,
+    "canReply": boolean,
+    "totalReplyCount": unsigned integer,
+    "isPublic": boolean
+  },
+  "replies": {
+    "comments": [
+      comments Resource ****
+    ]
+  }
+}
+
+Sample comments Resource object:
+{
+  "kind": "youtube#comment",
+  "etag": etag,
+  "id": string,
+  "snippet": {
+    "authorDisplayName": string,
+    "authorProfileImageUrl": string,
+    "authorChannelUrl": string,
+    "authorChannelId": {
+      "value": string
+    },
+    "channelId": string,
+    "videoId": string,
+    "textDisplay": string,
+    "textOriginal": string,
+    "parentId": string,
+    "canRate": boolean,
+    "viewerRating": string,
+    "likeCount": unsigned integer,
+    "moderationStatus": string,
+    "publishedAt": datetime,
+    "updatedAt": datetime
+  }
+}
+'''
---
+++ database.py
@@ -0,0 +1,76 @@
+'''
+Great medium article to set up PostgreSQL and pgAdmin (GUI for PostgreSQL)
+https://towardsdatascience.com/a-practical-guide-to-getting-set-up-with-postgresql-a1bf37a0cfd7
+
+Requirements:
+- download postgreSQL and pgAdmin 4 https://www.postgresql.org/download/ 
+- pip install SQLAlchemy (ORM, or object-relational mapping - generates SQL statements)
+- pip install psycopg2 (database driver - sends SQL statements to the database)
+- pip install pandas
+- pip install mysqlclient (supporst mySQLdb dialent and clients)
+
+'''
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pd
+import os
+import MySQLdb
+from secret import *
+
+class DBConnector():
+
+    # 89.27.154.69 closed port 5432 cannot establish TCP/IP connections
+    # def __init__(self, username="postgres", password="youtubenlp", host="localhost", port="5432", database="postgres") -> None:
+    #     self.con_url = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+    #     self.engine = create_engine(self.con_url)
+
+    def __init__(self) -> None:
+        self.con_url = f"mysql://{username}:{password}@{host}:{port}/{database}"
+        self.engine = create_engine(self.con_url)
+
+    def execute_sql_query(self, sql_query) -> None:
+        """ Executes a sql query"""
+        with self.engine.connect() as con:
+            con.execute(sql_query)
+
+    def execute_sql_from_file(self, sql_file) -> None:
+        """ Executes a sql query from a sql script/file """
+        script_name = sql_file
+        dirname = os.path.dirname(__file__)
+        sql_file = os.path.join(dirname, script_name)
+
+        sql = open(sql_file, 'r').read()
+
+        try:
+            # execute SQL commands
+            with self.engine.connect() as con:
+                con.execute(sql)
+            print(f'Successfully executed {script_name}')
+
+        except Exception as err:
+            print(f'Failed to execute {script_name}')
+            print(f'Error:\n{err}')
+
+    def insert_dataframe(self, dataframe, table_name) -> None:
+        """ Inserts pandas dataframe into a postgreSQL database table"""
+        dataframe.to_sql(
+            name = table_name, 
+            con = self.engine, 
+            if_exists = 'append', # 'fail' raises exception, 'replace' drops and recreates
+            index = True) # dataframe index as column
+
+    def fetch_dataframe_from_query(self, sql_query) -> pd.DataFrame:
+        """ Reads a select SQL query into a pandas dataframe"""
+        dataframe = pd.read_sql(sql_query, self.engine)
+        return dataframe
+
+    def fetch_rows_from_query(self, sql_query) -> list:
+        """ Reads a select SQL query and returns a list of tuples """
+        with self.engine.connect() as con:
+            result = con.execute(sql_query)
+        return result.fetchall()
+
+# dummy database
+database = DBConnector()
+df = database.fetch_dataframe_from_query("select * from d_chris order by id")
+print(df.head())
